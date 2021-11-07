a2af72376cfd6407cdc4c3e86db5da1f2331659f
Rubén Pérez <49503500+rubper@users.noreply.github.com>
2021-11-07 02:56:04 -0600

    bytes calculation lib changes

--- src/app/modules/mapping-policies/pages/direct/direct.component.ts
+++ src/app/modules/mapping-policies/pages/direct/direct.component.ts
@@ -1,5 +1,19 @@
-import { Component, ViewChild, Input, AfterViewInit, ElementRef } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  ViewChild,
+  Input,
+  AfterViewInit,
+  ElementRef,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { ByteUnit } from '@modules/mapping-policies/utils/types/byte-unit.type';
+import { MathFn } from '@shared/lib/math.functions';
 import { Position } from '@shared/types/position.type';
 import { BaseComponent } from '@shared/utils/base.component';
 import { AnimeService } from 'src/app/animations/anime.service';
@@ -9,14 +23,18 @@ declare var anime: any;
 @Component({
   selector: 'acs-direct',
   templateUrl: './direct.component.html',
-  styleUrls: ['./direct.component.css']
+  styleUrls: ['./direct.component.css'],
 })
 export class DirectComponent extends BaseComponent implements AfterViewInit {
   @ViewChild('memory') memoryBlock: ElementRef | undefined;
   @ViewChild('cache') cacheBlock: ElementRef | undefined;
+  MemoryAddr?: ByteUnit;
+  CacheSize?: ByteUnit;
   memoryInitialPosition?: Position;
   formGroup: FormGroup;
   bitsArray: number[] = [];
+  maxAddr: ByteUnit[] = [];
+  maxCacheAddr: ByteUnit[] = [];
 
   constructor(
     private readonly animeService: AnimeService,
@@ -24,46 +42,88 @@ export class DirectComponent extends BaseComponent implements AfterViewInit {
   ) {
     super();
     for (let i = 1; i <= 5; i++) {
-      this.bitsArray.push(Math.pow(2, i));      
+      this.bitsArray.push(Math.pow(2, i));
     }
+    this.bitsArray.push(10);
+    this.bitsArray.push(20);
+    this.bitsArray.push(30);
+    this.setMaxAddrArray();
+    console.log(this.maxAddr);
+
+    
     this.formGroup = this.formBuilder.group({
-      'memSize': [null, [Validators.required, BitsValidator(this.bitsArray)]],
-    })
+      memSize: [null, [Validators.required, ]],//BitsValidator(this.bitsArray)]],
+      cacheSize: [null, [Validators.required, ]],//BitsValidator(this.bitsArray)]],
+    });
   }
 
   ngAfterViewInit(): void {
     this.memoryInitialPosition = {
       left: `${this.MemoryBlock.offsetWidth + 100}px`,
-    }
+    };
+  }
+
+  setMaxAddrArray() {
+    let addr;
+    this.bitsArray.forEach((num, index) => {
+      addr = Math.pow(2, num);      
+      this.maxAddr[index] = {
+        value: addr,
+        readable: MathFn.getLowestIntegerByte(addr),
+        unit: MathFn.getByteMultiple(addr),
+      };  
+      this.maxCacheAddr[index] = {
+        value: addr,
+        readable: MathFn.getLowestIntegerByte(addr),
+        unit: MathFn.getByteMultiple(addr),
+      };
+    });
   }
 
   onSubmit($event: any) {
-    $event.preventDefault(); $event.stopPropagation();
+    $event.preventDefault();
+    $event.stopPropagation();
     console.log(this.MemorySize.value);
-    
+
     //this.animate();
     this.animeService.move('#memory', 100, 0);
     this.animeService.move('#cache', -200, 0);
   }
 
-  setMemoryAddrSize(elementRef: HTMLInputElement) {
+  setMemoryAddrSize(element: HTMLInputElement) {
+    this.bitsArray.push(+element.value);
+    this.setMaxAddrArray();
+    const addr = Math.pow(2, +element.value);
+
+    this.MemoryAddr = {
+      value: addr,
+      readable: MathFn.getLowestIntegerByte(addr),
+      unit: MathFn.getByteMultiple(addr), 
+    }
   }
 
   setCacheAddrSize(elementRef: HTMLInputElement) {
+    const addr = Math.pow(2, +elementRef.value);
+    this.CacheSize = {
+      value: addr,
+      readable: MathFn.getLowestIntegerByte(addr),
+      unit: MathFn.getByteMultiple(addr), 
+    }
   }
 
-  setWordSize(elementRef: HTMLInputElement) {
-  }
+  setWordSize(elementRef: HTMLInputElement) {}
 
   get MemoryBlock(): HTMLElement {
-    return this.memoryBlock?.nativeElement as HTMLElement || new HTMLElement();
+    return (
+      (this.memoryBlock?.nativeElement as HTMLElement) || new HTMLElement()
+    );
   }
-  
+
   get CacheBlock(): HTMLElement {
-    return this.cacheBlock?.nativeElement as HTMLElement || new HTMLElement();
+    return (this.cacheBlock?.nativeElement as HTMLElement) || new HTMLElement();
   }
 
   get MemorySize() {
-    return this.formGroup.get('memSize') as FormControl || new FormControl();
+    return (this.formGroup.get('memSize') as FormControl) || new FormControl();
   }
 }
--- src/app/shared/lib/math.functions.ts
+++ src/app/shared/lib/math.functions.ts
@@ -1,8 +1,31 @@
+import { UnitsDict, ByteUnits } from '@modules/mapping-policies/utils/types/byte-unit.type';
+
 export class MathFn {
   static isOdd(x: number) {
     return x % 2 === 0 ? false : true;
   }
   static isEven(x: number) {
     return !MathFn.isOdd(x);
+  }
+  static getLowestIntegerByte(x: number) {
+    let aux = x;
+    while (aux % 1024 === 0) {
+      aux = aux / 1024;
+    }
+    return aux;
+  }
+
+  static getByteMultiple(x: number): ByteUnits {
+    let aux = x;
+    let counter = 0;
+    while(aux % 1024 === 0) {
+      counter++;
+      aux = aux / 1024;
+    }
+    if (counter <= 8) {
+      
+      return UnitsDict[counter] as ByteUnits;
+    }
+    return 'B';
   }
 }
