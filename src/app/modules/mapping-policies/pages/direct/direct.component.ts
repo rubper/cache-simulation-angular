4730660d925ac7a272a2a2963718bba0e999244c
Rubén Pérez <49503500+rubper@users.noreply.github.com>
2021-11-06 20:03:26 -0600

    creates blocks that represents memories

--- src/app/modules/mapping-policies/pages/direct/direct.component.ts
+++ src/app/modules/mapping-policies/pages/direct/direct.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input, AfterViewInit, ElementRef } from '@angular/core';
 import { BaseComponent } from '@shared/utils/base.component';
 declare var anime: any;
 
@@ -7,7 +7,9 @@ declare var anime: any;
   templateUrl: './direct.component.html',
   styleUrls: ['./direct.component.css']
 })
-export class DirectComponent extends BaseComponent implements OnInit {
+export class DirectComponent extends BaseComponent implements AfterViewInit {
+  @ViewChild('memory') memoryBlock: ElementRef | undefined;
+  @ViewChild('cache') cacheBlock: ElementRef | undefined;
   @ViewChild('memoryAddrSize') memoryAddrSize: Input | undefined;
   @ViewChild('cacheSize') cacheSize: Input | undefined;
   movementToggle: boolean = false;
@@ -16,7 +18,8 @@ export class DirectComponent extends BaseComponent implements OnInit {
     super();
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.CacheBlock.style.left = `${this.MemoryBlock.offsetWidth + 100}px`;
   }
 
   onSubmit($event: any) {
@@ -25,20 +28,55 @@ export class DirectComponent extends BaseComponent implements OnInit {
     this.animate();
   }
 
-  setMemoryAddrSize(event: any) {
-    console.log(event);
+  setMemoryAddrSize(elementRef: HTMLInputElement) {
   }
 
-  setCacheAddrSize(event: any) {
-    console.log(event);
+  setCacheAddrSize(elementRef: HTMLInputElement) {
+  }
+
+  setWordSize(elementRef: HTMLInputElement) {
   }
 
   animate() {
     anime.timeline()
     .add({
-      targets: '#obj1',
-      translateX: this.movementToggle? '75%' : 0,
+      targets: '#memory',
+      translateX: this.movementToggle? '100px' : 0,
     } as anime.AnimeParams)
   }
 
+  scaleX(target: string, value: number) {
+    anime.timeline()
+    .add({
+      targets: target,
+      scaleX: value,
+    } as anime.AnimeParams)
+  }
+
+  
+  scaleY(target: string, value: number) {
+    anime.timeline()
+    .add({
+      targets: target,
+      scaleY: value,
+    } as anime.AnimeParams)
+  }
+
+  
+  scale(target: string, x: number, y: number) {
+    anime.timeline()
+    .add({
+      targets: target,
+      scaleX: x,
+      scaleY: y,
+    } as anime.AnimeParams)
+  }
+
+  get MemoryBlock(): HTMLElement {
+    return this.memoryBlock?.nativeElement as HTMLElement || new HTMLElement();
+  }
+  
+  get CacheBlock(): HTMLElement {
+    return this.cacheBlock?.nativeElement as HTMLElement || new HTMLElement();
+  }
 }
