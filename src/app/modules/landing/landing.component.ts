6f06c5bc64cb1db13a3ece0e6b47048be5cfce7d
Rubén Pérez <49503500+rubper@users.noreply.github.com>
2021-11-14 16:05:51 -0600

    fixes landing page with working nav links

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PolicyType } from '@shared/types/policy-type.type';
+import { NavLink } from './utils/types/nav-link.type';
 
 @Component({
   selector: 'acs-landing',
@@ -6,6 +8,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  links: NavLink[] = [
+    {
+      name: 'mapping' as PolicyType,
+      title: 'Ubicación',
+      subLinks: [
+        {
+          name: 'direct',
+          title: 'Directa',
+        },
+        {
+          name: 'associative',
+          title: 'Asociativa',
+        },
+        {
+          name: 'set-associative',
+          title: 'Asociativa por conjuntos',
+        },
+      ]
+    },
+    {
+      name: 'fetching' as PolicyType,
+      title: 'Extraccion',
+      subLinks: [
+        {
+          name: 'prefetch',
+          title: 'Demanda',
+        },
+        {
+          name: 'request',
+          title: 'Anticipacion',
+        },
+        {
+          name: 'selective',
+          title: 'Selectiva',
+        },
+      ]
+    },
+    {
+      name: 'replacement' as PolicyType,
+      title: 'Remplazo',
+      subLinks: [
+        {
+          name: 'random',
+          title: 'Aleatorio',
+        },
+        {
+          name: 'lru',
+          title: 'Least Recently Used',
+        },
+        {
+          name: 'fifo',
+          title: 'First In First Out',
+        },
+      ]
+    },
+    {
+      name: 'writing' as PolicyType,
+      title: 'Escritura',
+      subLinks: [
+        {
+          name: 'write-through',
+          title: 'Inmediata',
+        },
+        {
+          name: 'write-back',
+          title: 'Aplazada',
+        },
+        {
+          name: 'write-with-alocate',
+          title: 'Con Ubicacion',
+        },
+        {
+          name: 'write-with-no-alocate',
+          title: 'Sin Ubicacion',
+        },
+      ]
+    },
+  ];
 
   constructor() { }
 
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from '../landing/landing.component';
 import { LayoutsModule } from '@layouts/layouts.module';
+import { NavCardComponent } from './utils/nav-card/nav-card.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRippleModule } from '@angular/material/core';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    NavCardComponent
   ],
   imports: [
   CommonModule,
     LandingRoutingModule,
-    LayoutsModule
+    LayoutsModule,
+    MatGridListModule,
+    MatRippleModule,
   ]
 })
 export class LandingModule { }
