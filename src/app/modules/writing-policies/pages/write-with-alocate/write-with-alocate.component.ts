aa4f5bc3d2ccccd0bfecd4dd12409f94fbe9b480
JEfrainDR <djoseefrainr@gmail.com>
2021-11-14 19:14:11 -0600

    write with allocate

--- src/app/modules/writing-policies/pages/write-with-alocate/write-with-alocate.component.ts
+++ src/app/modules/writing-policies/pages/write-with-alocate/write-with-alocate.component.ts
@@ -9,33 +9,85 @@ export class WriteWithAlocateComponent implements OnInit {
 
   play = false;
   icon = 'play_arrow';
-  celdasMP = [
-    'Bloque 1',
-    'Bloque 2',
-    'Bloque 3',
-    'Bloque 4',
-    'Bloque 5',
-    'Bloque 6',
-    'Bloque 7',
-    'Bloque 8',
-  ];
+  falloEscrituraFlag = false;
+  celdasMP: string[] = [];
   celdasMC: string[] = [];
   logs: string[] = [];
 
+  direccionesMP: string [] = [];
+  dataMP: string[] = [];
+
+  direccionesEnMC: string[] = [];
+  dataMC: string[] = [];
+  dirtyBit: number[] = [];
+
   celdaInput = true;
   bloqueSolicitado = 0;
+  nuevoValorBloqueSolicitado = '';
 
   constructor() { }
 
   ngOnInit(): void {
     // this.getRandomItem();
+    this.generarDireccionesMp();
+  }
+
+  generarDireccionesMp(): void {
+    const salida: number[] = [];
+    let contador = 0;
+    for ( let i = 0; i <= 1; i ++ ) {
+      salida[0] = i;
+      for ( let j = 0; j <= 1; j ++ ) {
+        salida[1] = j;
+        for ( let k = 0; k <= 1; k ++ ) {
+          salida[2] = k;
+          for ( let l = 0; l <= 1; l ++ ) {
+            salida[3] = l;
+            for ( let m = 0; m <= 1; m ++ ) {
+              salida[4] = m;
+              for ( let n = 0; n <= 1; n ++ ) {
+                salida[5] = n;
+                for ( let p = 0; p <= 1; p ++ ) {
+                  salida[6] = p;
+                  for ( let q = 0; q <= 1; q ++ ) {
+                    salida[7] = q;
+                    this.celdasMP.push('Bloque ' + contador);
+                    this.dataMP.push(this.generateData(5));
+                    contador += 1;
+                    this.addToDirectionsMp(salida);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  generateData(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i ++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  addToDirectionsMp( array: number[] ): void {
+    let numero = '';
+    for ( const item of array ) {
+      numero += item;
+    }
+    this.direccionesMP.push(numero);
   }
 
   getRandomItem(): void {
     if ( this.play ) {
       setTimeout(() => {
         this.randomItem();
-      }, 5000);
+      }, 6000);
     }
   }
 
@@ -47,16 +99,25 @@ export class WriteWithAlocateComponent implements OnInit {
       this.getRandomItem();
     } else {
       this.addToCache(this.bloqueSolicitado);
-      this.getRandomItem();
+      this.generateNewValue();
+      setTimeout(() => {
+        this.updateDataMainValue();
+      }, 6500);
     }
   }
 
   addToCache(index: number): void {
     if (this.celdasMC.length <= 3) {
       this.celdasMC.push(this.celdasMP[index]);
+      this.direccionesEnMC.push(this.direccionesMP[index]);
+      this.dataMC.push(this.dataMP[index]);
+      this.dirtyBit.push(0);
     } else {
       this.deleteFromCacheRandom();
       this.celdasMC.push(this.celdasMP[index]);
+      this.direccionesEnMC.push(this.direccionesMP[index]);
+      this.dataMC.push(this.dataMP[index]);
+      this.dirtyBit.push(0);
     }
   }
 
@@ -64,14 +125,72 @@ export class WriteWithAlocateComponent implements OnInit {
     const bloqueDelete = Math.floor(Math.random() * this.celdasMC.length);
     this.pushLog( this.celdasMC[bloqueDelete] + ' eliminado de caché...');
     this.celdasMC.splice(bloqueDelete, 1);
+    this.direccionesEnMC.splice(bloqueDelete, 1);
+    this.dataMC.splice(bloqueDelete, 1);
+    this.dirtyBit.splice(bloqueDelete, 1);
+  }
+
+  deleteFromCache( bloque: number ): void {
+    this.pushLog( this.celdasMC[bloque] + ' eliminado de caché...');
+    this.celdasMC.splice( bloque, 1 );
+    this.direccionesEnMC.splice( bloque, 1 );
+    this.dataMC.splice( bloque, 1 );
   }
 
   pushLog(comentario: string): void {
-    if ( this.logs.length <= 7 ) {
-      this.logs.push(comentario);
+    this.logs.push(comentario);
+  }
+
+  generateNewValue(): void {
+    const probabilidad = Math.random();
+    if ( probabilidad < 0.50 ) {
+      // Generación de un nuevo valor de forma aleatoria
+      this.nuevoValorBloqueSolicitado = this.generateData(5);
+      this.pushLog( 'Nuevo valor del ' + this.celdasMP[this.bloqueSolicitado] + ': ' + this.nuevoValorBloqueSolicitado );
+      // Actualización del valor luego de dos segundos
+      setTimeout(() => {
+        this.pushLog('Actualizando valor en MC...');
+        this.updateDataCacheValue( this.nuevoValorBloqueSolicitado, this.bloqueSolicitado );
+      }, 5000);
+    }
+  }
+
+  updateDataMainValue(): void {
+    if ( !this.falloEscrituraFlag ) {
+      this.getRandomItem();
+      const celdaMP = this.celdasMP[this.bloqueSolicitado];
+      const indexMC = this.celdasMC.indexOf(celdaMP, 0);
+      if ( this.dirtyBit[indexMC] === 1 ) {
+        this.pushLog('El dato del ' + this.celdasMP[this.bloqueSolicitado] + ' ha sido modificado...');
+        setTimeout(() => {
+          this.pushLog('Actualizando valor de ' + this.celdasMP[this.bloqueSolicitado] + ' en MP...');
+          this.dataMP[this.bloqueSolicitado] = this.nuevoValorBloqueSolicitado;
+        }, 2000);
+      } else {
+        this.pushLog('El dato del ' + this.celdasMP[this.bloqueSolicitado] + ' no ha sido modificado...');
+      }
     } else {
-      this.logs.splice(1, 1);
-      this.logs.push(comentario);
+      this.pushLog('Obteniendo el ' + this.celdasMP[this.bloqueSolicitado] + ' de MP a MC');
+      this.addToCache(this.bloqueSolicitado);
+      setTimeout(() => {
+        this.pushLog('Actualizando el ' + this.celdasMP[this.bloqueSolicitado] + ' de MC');
+        this.updateDataCacheValue( this.nuevoValorBloqueSolicitado, this.bloqueSolicitado );
+        this.falloEscrituraFlag = false;
+        setTimeout(() => {
+          this.updateDataMainValue();
+        }, 1000);
+      }, 1000);
+    }
+  }
+
+  updateDataCacheValue( value: string, index: number ): void {
+    const indexMC = this.dataMC.indexOf(this.dataMP[index], 0);
+    if ( indexMC > -1 ) {
+      this.dataMC[indexMC] = value;
+      this.dirtyBit[indexMC] = 1;
+    } else {
+      this.pushLog('Fallo de escritura...');
+      this.falloEscrituraFlag = true;
     }
   }
 
